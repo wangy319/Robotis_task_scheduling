0ce3f073706d1931393c2ea758041b4c9242779b
wangy319 <yingxue.sherry.wang@gmail.com>
2017-10-24 11:43:25 -0400

    refresh

--- run_assembly_example_1.sh
+++
@@ -1,4 +0,0 @@
-cd MiniZinc;
-mzn-g12fd -a -s Assembly_example_1.mzn Assembly_example_1.dzn -o ../Assemble_example_1_dependencies_output;
-cd ..;
-java -jar SchedPrinter.jar Assemble_example_1_dependencies_output > Assemble_example_1_visual;
